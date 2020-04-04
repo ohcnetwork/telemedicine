f7fd3b24c310e56b5e7c30a5d54102473982b06a
tonydavis1995 <tony@ensologic.com>
2020-04-04 15:52:07 +0530

    https reports

--- src/store/actions/ExecuteData.js
+++ src/store/actions/ExecuteData.js
@@ -23,7 +23,7 @@ let executeDataConfig = {
   GET_ALL_USERS: {url : "https://tele.coronasafe.in/api/user/all/get"},
   DISTRICT_LIST: {url : "https://api.care.coronasafe.in/api/v1/state/1/districts/"},
   LOCAL_BODY_LIST: {url: "https://api.care.coronasafe.in/api/v1/local_body/"},
-  RISK_RESULT_DATA: {url: "http://volunteer.coronasafe.network/api/reports"},
+  RISK_RESULT_DATA: {url: "https://volunteer.coronasafe.network/api/reports"},
   REQUEST_CALL_USER: { url: "https://tele.coronasafe.in/api/user/schedule"}
 };
 
